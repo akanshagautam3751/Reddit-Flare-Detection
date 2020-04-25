aecae66c18f65e55207964d8263ac7b394e76ee1
Akansha Gautam <akansha16221@iiitd.ac.in>
2020-04-25 21:24:32 +0530

    test.py updated

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:5000/automated_testing"
+url = "https://reddit-flare-detection.herokuapp.com/automated_testing"
 files = {'upload_file': open('sample_test_file.txt', 'rb')}
 r = requests.post(url, files=files)
 print (r.text)
